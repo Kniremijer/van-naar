bc3604adcfcb9ed1c598584447245fcaeee01387
Kniremijer <98964642+Kniremijer@users.noreply.github.com>
2025-08-01 14:32:24 +0000

    layout in layout bestand

--- site/src/data/units.ts
+++ site/src/data/units.ts
@@ -1,6 +1,12 @@
 export const units = {
-  lengte: ['mm', 'cm', 'dm', 'm', 'dam', 'hm', 'km'],
-  temperatuur: ['c', 'f', 'k'],
+  lengte: ['mm', 'cm', 'm', 'km', 'in', 'ft', 'yd', 'mi'],
+  gewicht: ['mg', 'g', 'kg', 't', 'oz', 'lb'],
+  temperatuur: ['C', 'F', 'K'],
+  volume: ['ml', 'l', 'm3', 'floz', 'cup', 'pt', 'qt', 'gal', 'tsp', 'tbsp'],
+  tijd: ['ms', 's', 'min', 'h', 'd'],
+  energie: ['J', 'kJ', 'cal', 'kcal', 'Wh', 'kWh'],
+  snelheid: ['m/s', 'km/h', 'mph', 'ft/s', 'knot'],
+  oppervlakte: ['mm2', 'cm2', 'm2', 'km2', 'in2', 'ft2', 'yd2', 'acre', 'ha']
 } as const;
 
 export type Category = keyof typeof units;
